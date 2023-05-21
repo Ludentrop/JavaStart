7ee201edf63de8d7a6ad6fb600f65e2146377bf4
Maier Ludentrop <agamemnonagross@gmail.com>
2023-05-21 15:26:08 +0300

    finished chapter02

--- src/main/java/com/maier/javacore/chapter02/Example.java
+++ src/main/java/com/maier/javacore/chapter02/Example.java
@@ -2,6 +2,6 @@ package com.maier.javacore.chapter02;
 
 class Example {
     public static void main(String[] args) {
-        System.out.println("hello hell!");
+        System.out.println("Hello, Hell!");
     }
 }
